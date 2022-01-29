0055c960bd9d5d22fbe48e1e6d4909a937ba28a3
Jonathan Paugh <j.d.paugh@gmail.com>
2022-01-29 13:58:33 -0800

    CSharp Web Server -Now runs on my CSharp web server -Changed file hierarchy

---
+++ src/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JapeCore;
+using JapeService;
+
+namespace JonathanPaugh
+{
+    internal class Program : ConsoleProgram<int, int>
+    {
+        protected override string DefaultLog => "server.log";
+
+        private int http;
+        private int https;
+
+        private static async Task Main(string[] args) => await RunAsync<Program>(args);
+
+        protected override IEnumerable<ICommandArg> Args() => Service.Args;
+
+        protected override void OnSetup(int http, int https)
+        {
+            this.http = http;
+            this.https = https;
+        }
+
+        protected override async Task OnStartAsync()
+        {
+            WebServer webServer = new(http, https);
+            await webServer.Start();
+        }
+    }
+}
---
+++ src/WebServer.cs
@@ -0,0 +1,7 @@
+﻿namespace JonathanPaugh
+{
+    public class WebServer : JapeWeb.WebServer
+    {
+        public WebServer(int http, int https) : base(http, https) {} 
+    }
+}
