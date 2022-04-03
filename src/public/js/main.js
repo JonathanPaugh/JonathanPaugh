8acfcb4be65c5a06be5fbe0aeddcbdf1d095843c
Jonathan Paugh <j.d.paugh@gmail.com>
2022-04-03 16:03:01 -0700

    Projects From Data -Project item template is created from project-item.xml -Project items are now dynamically created from data in the projects.json file -Added placeholder data for projects -Added some preview images for projects -Added vector icons -Added stacking game submodule in portfolio

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,37 +1,58 @@
 cachedIcons = {};
 
 $(() => {
-    updateFooterYear();
-    createFooterTransition();
+    createSocialIcons();
     createTechnologyIcons();
-    createSocialIcons()
+    createProjectItems();
+    createFooterTransition();
+    updateFooterYear();
 });
 
-function updateFooterYear() {
-    $(".footer-year").append(new Date().getFullYear());
+
+
+async function createProjectItems() {
+    let data = await fetchFileAsync("./JonathanPaugh/src/public/data/projects.json");
+    let template = await fetchFileAsync("./JonathanPaugh/src/template/project-item.xml");
+    projects = JSON.parse(data);
+    for (let [heading, project] of Object.entries(projects)) {
+        await createProjectItem(heading, project, template);
+    }
+}
+
+async function createProjectItem(heading, project, template) {
+    template = $(template).appendTo(".project-container");
+    template.find(".project-item-link").attr("href", project.url);
+
+    template.find(".project-item-heading").html(heading);
+    template.find(".project-item-title").html(project.title);
+    template.find(".project-item-type").html(`${project.type}, `);
+    template.find(".project-item-year").html(project.year);
+
+    preview = createElement("img");
+    preview.attr("src", project.preview);
+
+    template.find(".project-item-preview").append(preview);
 }
 
 async function createSocialIcons() {
     let data = await fetchFileAsync("./JonathanPaugh/src/public/data/socials.json");
     socials = JSON.parse(data);
-
-    for (let [file, link] of Object.values(socials)) {
-        await createSocialIcon(file, link);
+    for (let [file, url] of Object.values(socials)) {
+        await createSocialIcon(file, url);
     }
 }
 
-async function createSocialIcon(file, link) {
+async function createSocialIcon(file, url) {
     let data = await fetchFileAsync(`./JonathanPaugh/src/public/svg/socials/${file}`);
-    let container = createElement("a");
-    container.attr("href", link);
-    $(".splash-icons").append(container);
-    container.append(data);
+    let link = createElement("a");
+    link.attr("href", url);
+    $(".splash-icons").append(link);
+    link.append(data);
 }
 
 async function createTechnologyIcons() {
     let data = await fetchFileAsync("./JonathanPaugh/src/public/data/technologies.json");
     technologies = JSON.parse(data);
-
     for (let [name, file] of Object.entries(technologies)) {
         await createTechnologyIcon(name, file);
     }
@@ -43,20 +64,20 @@ async function createTechnologyIcon(name, file) {
     let container = createDiv();
     $(".about-technologies-container").append(container);
 
-    let anchor = createElement("a");
-    container.append(anchor);
-    anchor.append(data);
+    let link = createElement("a");
+    container.append(link);
+    link.append(data);
 
     let tooltip = createDiv();
     tooltip.addClass("tooltip");
     tooltip.html(name);
-    anchor.before(tooltip);
+    link.before(tooltip);
 
-    Popper.createPopper(anchor[0], tooltip[0], {
+    Popper.createPopper(link[0], tooltip[0], {
         placement: "bottom"
     });
 
-    anchor.hover(() => {
+    link.hover(() => {
         tooltip.addClass("active");
     }, () => {
         tooltip.removeClass("active");
@@ -67,4 +88,8 @@ function createFooterTransition() {
     fetchFile("./JonathanPaugh/src/public/svg/transition.svg", (data) => {
         $(".transition").append(data);
     });
+}
+
+function updateFooterYear() {
+    $(".footer-year").append(new Date().getFullYear());
 }
